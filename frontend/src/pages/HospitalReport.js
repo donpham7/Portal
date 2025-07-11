35f7a7187bcdc1b14c532a301e46cf3faf054aed
pham0444 <pham0444@umn.edu>
2025-07-10 22:39:47 -0500

    doctor dash to report

---
+++ frontend/src/pages/HospitalReport.js
@@ -0,0 +1,10 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Lin,
+  useNavigate,
+} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+
+export default function HospitalReport() {}
