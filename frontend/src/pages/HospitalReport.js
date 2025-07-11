8f91d199e94576c9c147dca7ee2f5d6de3229445
pham0444 <pham0444@umn.edu>
2025-07-10 23:09:36 -0500

    report doctro

--- frontend/src/pages/HospitalReport.js
+++ frontend/src/pages/HospitalReport.js
@@ -4,7 +4,40 @@ import {
   Route,
   Lin,
   useNavigate,
+  useParams,
 } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 
-export default function HospitalReport() {}
+export default function HospitalReport() {
+  const { user_id, report_name } = useParams();
+  const [report, setReport] = useState(null);
+  console.log();
+  useEffect(() => {
+    const report_url = "/api/get_report/" + user_id + "/" + report_name;
+    fetch(report_url)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setReport(data);
+      })
+      .catch((err) => {
+        // Error message
+        console.log(err);
+      });
+  }, []); // Empty dependency array = run only once on mount
+  return (
+    <div class="columns">
+      <div class="column">
+        <p>User ID: {user_id}</p>
+      </div>
+      <div class="column">
+        <p>Report ID: {report_name}</p>
+      </div>
+    </div>
+  );
+}
